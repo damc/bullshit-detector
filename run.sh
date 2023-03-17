4dde6af47c642a40309b7b910767f4f1e344913a
Damian Czapiewski <damianczap@outlook.com>
2023-03-17 10:51:41 +0000

    Save data to database

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 export FLASK_APP=website
 export FLASK_DEBUG=1
 export SECRET_KEY='ilovejustinbieber'
+export DATABASE_URI='mysql://root:dijkstr4@localhost:3306/bullshit_detector'
 flask run --port 1234
